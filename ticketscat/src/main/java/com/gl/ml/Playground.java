daf26aa8e8961d99797d37b90c3093f7aa103b61
Roman Loyko <Roman.Loyko@synapse.com>
2017-07-26 12:30:19 +0300

    Tokenization and Word2Vec implementation

--- ticketscat/src/main/java/com/gl/ml/Playground.java
+++ ticketscat/src/main/java/com/gl/ml/Playground.java
@@ -1,11 +1,17 @@
 package com.gl.ml;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.ml.feature.Tokenizer;
+import org.apache.spark.ml.feature.Word2Vec;
+import org.apache.spark.ml.feature.Word2VecModel;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import scala.collection.mutable.WrappedArray;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +20,8 @@ import java.util.List;
  */
 public class Playground {
     public static void main(String[] args) {
-        System.setProperty("hadoop.home.dir", "c:\\hadoop\\");
+        ClassLoader classLoader = Playground.class.getClassLoader();
+        System.setProperty("hadoop.home.dir", classLoader.getResource("hadoop").getFile());
         SparkSession spark = SparkSession
                 .builder()
                 .appName("SampleFeatures")
@@ -32,15 +39,31 @@ public class Playground {
         //1. feature extraction
         List<Row> data = new ArrayList<>();
 
+
+        File file = new File(classLoader.getResource("tickets_input.csv").getFile());
+
         Dataset<Row> dataset = spark.read()
                 .format("com.databricks.spark.csv")
                 .option("header", "true")
-                .csv("src/main/resources/kickstarter/train_red.csv");
+                .csv(file.getAbsolutePath());
+
+        Tokenizer tokenizer = new Tokenizer().setInputCol("DESCRIPTION").setOutputCol("desc_words");
+//        spark.udf().register("countTokens", (WrappedArray<?> words) -> words.size(), DataTypes.IntegerType);
+        Dataset<Row> tokenized = tokenizer.transform(dataset);
+        tokenized.select("DESCRIPTION", "desc_words").show(false);
 
 
-        Dataset<Row> dataset2 = spark.createDataFrame(
-                dataset.javaRDD().map(row -> RowFactory.create(((String) row.get(4)).split("-"))),
-                dataset.schema());
+        Word2Vec word2Vec = new Word2Vec()
+                .setInputCol("desc_words")
+                .setOutputCol("vectorized");
+        Word2VecModel model = word2Vec.fit(tokenized);
+        Dataset<Row> vectorized = model.transform(tokenized);
+        vectorized.select("DESCRIPTION", "desc_words", "vectorized").show(false);
+
+//
+//        Dataset<Row> dataset2 = spark.createDataFrame(
+//                dataset.javaRDD().map(row -> RowFactory.create(((String) row.get(4)).split("-"))),
+//                dataset.schema());
 
 
 //        InputStream inputStream = ClassLoader.getSystemResourceAsStream("kickstarter/train_red.csv");
@@ -61,11 +84,12 @@ public class Playground {
 //        });
 //
 //        Dataset<Row> documentDF = spark.createDataFrame(data, schema);
-
-        System.out.println(dataset2.columns()[4]);
-        for (Row row : dataset2.takeAsList(10)) {
-            System.out.println(row.getString(4));
-        }
+//
+//        System.out.println(dataset2.columns()[4]);
+//        for (Row row : dataset2.takeAsList(10)) {
+//            System.out.println(row.getString(4));
+//        }
 
     }
+
 }
